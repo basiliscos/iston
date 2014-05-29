0d3da2e779fe2a16573e927153e958a6e9279198
Ivan Baidakou <dmol@gmx.com>
2014-05-30 01:25:52 +0300

    Build results of a34c577 (on master)

--- lib/Iston/Object/ObservationPath.pm
+++ lib/Iston/Object/ObservationPath.pm
@@ -1,12 +1,17 @@
 package Iston::Object::ObservationPath;
 $Iston::Object::ObservationPath::VERSION = '0.01';
 use 5.12.0;
+use utf8;
 
 use Function::Parameters qw(:strict);
+use List::Util qw/reduce/;
+use List::MoreUtils qw/pairwise/;
 use Moo;
 use Math::MatrixReal;
+use Math::Trig;
 use OpenGL qw(:all);
 
+use aliased qw/Iston::Vector/;
 use aliased qw/Iston::Vertex/;
 
 with('Iston::Drawable');
@@ -14,11 +19,13 @@ with('Iston::Drawable');
 my $_PI = 2*atan2(1,0);
 my $_G2R = $_PI / 180;
 
-has history  => (is => 'ro', required => 1);
-has scale    => (is => 'rw', default => sub { 1; });
-has rotation => (is => 'rw', default => sub { [0, 0, 0] });
-has vertices => (is => 'rw');
-has indices  => (is => 'rw');
+has history     => (is => 'ro', required => 1);
+has scale       => (is => 'rw', default => sub { 1; });
+has rotation    => (is => 'rw', default => sub { [0, 0, 0] });
+has vertices    => (is => 'rw');
+has indices     => (is => 'rw');
+has index_at    => (is => 'rw', default => sub{ {} });
+has active_time => (is => 'rw');
 
 method BUILD {
     $self->_build_vertices_and_indices;
@@ -26,13 +33,14 @@ method BUILD {
 
 method _build_vertices_and_indices {
     my $history = $self->history;
-    my $current_point =  Math::MatrixReal->new_from_rows([ [0], [0], [1] ]);
+    my $current_point =  Math::MatrixReal->new_from_cols([ [0, 0, 1] ]);
     my ($x_axis_degree, $y_axis_degree) = (0, 0);
     my @vertices;
     my @indices;
     for my $i (0 .. $history->elements-1) {
         my $record = $history->records->[$i];
-        my ($dx, $dy) = map { $record->$_ } qw/x_axis_degree y_axis_degree/;
+        my ($dx, $dy, $timestamp) = map { $record->$_ }
+            qw/x_axis_degree y_axis_degree timestamp/;
         $x_axis_degree = $dx * -1;
         $y_axis_degree = $dy * -1;
         my $r_a = Math::MatrixReal->new_from_rows([
@@ -51,9 +59,42 @@ method _build_vertices_and_indices {
         my $v = Vertex->new([$x, $y, $z]);
         push @vertices, $v;
         push @indices, $i-1, $i if($i);
+        $self->index_at->{$timestamp} = $i;
     }
     $self->vertices(\@vertices);
     $self->indices(\@indices);
+};
+
+method arrow_vertices($index_to, $index_from) {
+    my ($start, $end) = map { $self->vertices->[$_] } ($index_from, $index_to);
+    my $direction =  $start->vector_to($end);
+    my $d_normal = Vector->new([@$direction])->normalize;
+    my $n = Vector->new([0, 1, 0]);
+    my $scalar = reduce { $a + $b } pairwise { $a * $b} @$d_normal, @$n;
+    my $f = acos($scalar);
+    my $axis = ($n * $d_normal)->normalize;
+    my ($x, $y, $z) = @$axis;
+    my $rotation = Math::MatrixReal->new_from_rows([
+        [cos($f)+(1-cos($f))*$x**2,    (1-cos($f))*$x*$y-sin($f)*$z, (1-cos($f))*$x*$z+sin($f)*$y ],
+        [(1-cos($f))*$y*$z+sin($f)*$z, cos($f)+(1-cos($f))*$y**2 ,   (1-cos($f))*$y*$z-sin($f)*$x ],
+        [(1-cos($f))*$z*$x-sin($f)*$y, (1-cos($f))*$z*$y+sin($f)*$x, cos($f)+(1-cos($f))*$z**2    ],
+    ]);
+    my @normals = map { Vector->new($_) }
+        ([1, 0, 0], [0, 0, -1], [-1, 0, 0], [0, 0, 1]);
+    my $length = $direction->length;
+    my @results =
+        map {
+            for my $i (0 .. 2) {
+                $_->[$i] += $start->[$i] 
+            }
+            $_;
+        }
+        map { $_ * $length }
+        map {
+            my $r = $rotation * Math::MatrixReal->new_from_cols([ [@$_] ]);
+            my $result_vector = Vector->new( [map { $r->element($_, 1) } (1 .. 3) ] );
+        } @normals;
+    return @results;
 }
 
 method draw {
@@ -69,13 +110,39 @@ method draw {
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer_p(3, $vertices);
 
-    # applying material properties to the whole object
-    glMaterialfv_s(GL_FRONT, GL_DIFFUSE,   pack("f4", 0.45, 0.0, 0,0, 1.0 ));
-    glMaterialfv_s(GL_FRONT, GL_AMBIENT,   pack("f4", 0.45, 0.0, 0,0, 1.0 ));
-    glMaterialfv_s(GL_FRONT, GL_EMISSION,  pack("f4", 0.45, 0.0, 0,0, 1.0 ));
-
     my $indices = $self->indices;
-    glDrawElements_p(GL_LINES, @$indices);
+    my @passive_indices = @$indices;
+
+    # hilight recent active path
+    my $active_time = $self->active_time;
+    if (defined $active_time && exists $self->index_at->{$active_time}) {
+        my $last_index = $self->index_at->{$active_time};
+        my $count = $last_index >= 5 ? 5 : $last_index;
+        if ($count) {
+            my $color = pack("f4", 0.0, 0.95, 0.0, 1.0);
+            glMaterialfv_s(GL_FRONT, GL_DIFFUSE,   $color);
+            glMaterialfv_s(GL_FRONT, GL_AMBIENT,   $color);
+            glMaterialfv_s(GL_FRONT, GL_EMISSION,  $color);
+            my @active_indices = splice(@passive_indices, ($last_index-$count)*2, $count*2);
+            glDrawElements_p(GL_LINES, @active_indices);
+
+            my @arrows = $self->arrow_vertices($last_index, $last_index-1);
+            my $top = $self->vertices->[$last_index];
+            my $arrow_vertices = OpenGL::Array->new_list(
+                GL_FLOAT,
+                map { @$_ } ($top, @arrows)
+            );
+            my @arrow_indices = (1, 0, 2, 0, 3, 0, 4, 0);
+            glVertexPointer_p(3, $arrow_vertices);
+            glDrawElements_p(GL_LINES, @arrow_indices);
+        }
+    }
+
+    glVertexPointer_p(3, $vertices);
+    glMaterialfv_s(GL_FRONT, GL_DIFFUSE,   pack("f4", 0.45, 0.0, 0.0, 1.0 ));
+    glMaterialfv_s(GL_FRONT, GL_AMBIENT,   pack("f4", 0.45, 0.0, 0.0, 1.0 ));
+    glMaterialfv_s(GL_FRONT, GL_EMISSION,  pack("f4", 0.45, 0.0, 0.0, 1.0 ));
+    glDrawElements_p(GL_LINES, @passive_indices);
 }
 
 1;
