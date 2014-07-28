4ba5f5e448a92e5b7aee328f6bce128936ce308f
Ivan Baidakou <dmol@gmx.com>
2014-07-28 11:36:42 +0300

    Use more older strftime format string standards to be working on Windows

--- lib/Iston/Application/Analyzer.pm
+++ lib/Iston/Application/Analyzer.pm
@@ -272,7 +272,7 @@ sub _build_menu {
         my @history_names = (
             "choose history",
             map { /^history_(\d+)_(.+)\.csv$/
-                ? strftime('%F %T', localtime($1)) . " ($1)"
+                ? strftime('%Y-%m-%d %H:%M:%S', localtime($1)) . " ($1)"
                 : $_
             }
             map { $_->basename } @{ $_->{histories} },
