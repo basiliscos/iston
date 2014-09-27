48cff6ab6344e19e839c2e10a9f2ca018397e734
Ivan Baidakou <dmol@gmx.com>
2014-09-27 15:29:32 +0300

    Compute colour using global ambient and ambient components of material and light source

--- share/shaders/object.fragment.glsl
+++ share/shaders/object.fragment.glsl
@@ -15,25 +15,31 @@ struct lightSource
 {
   vec4 position;
   vec4 diffuse;
+  vec4 ambient;
 };
 
 struct material
 {
   vec4 diffuse;
+  vec4 ambient;
 };
 
 // defaults
 
 lightSource light1 = lightSource(
-  vec4(20, 5, 20, 0),
-  vec4(0.8, 0.8, 0.8, 0)
+  vec4(20, 5, 20, 0),              // position
+  vec4(0.8, 0.8, 0.8, 0),          // diffuse
+  vec4(0.1, 0.1, 0.1, 0)           // ambient
 );
 
 material frontMaterial = material(
-  vec4(0.8, 0.8, 0.8, 0)
+  vec4(0.8, 0.8, 0.8, 0),           // diffuse
+  vec4(0.2, 0.2, 0.2, 0)            // ambient
 );
 
 uniform vec4 default_color = vec4(0.8, 0.8, 0.8, 1);
+uniform vec4 global_ambient = vec4(0.5, 0.5, 0.1, 1); // yellow abmient
+
 
 // functions
 
@@ -42,8 +48,11 @@ vec4 compute_color(vec4 initial_color) {
   vec4 light_dir = normalize(light1.position);
   vec3 light_dir3 = vec3(light_dir.x, light_dir.y, light_dir.z);
   float intensity = max(dot(normal, light_dir3), 0);
+
   vec4 diffuse = frontMaterial.diffuse * light1.diffuse;
-  vec4 result = intensity * diffuse * initial_color;
+  vec4 ambient = frontMaterial.ambient * (light1.ambient + global_ambient);
+
+  vec4 result = (intensity * diffuse * initial_color) + (ambient * initial_color);
   return result;
 }
 
