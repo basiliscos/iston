ff0a05d8a4d0a3d5e73ab0ff1fe599b08242d8da
Ivan Baidakou <dmol@gmx.com>
2014-10-12 20:10:21 +0300

    Minor performance improvements

--- lib/Iston/Loader.pm
+++ lib/Iston/Loader.pm
@@ -40,7 +40,7 @@ method load {
             # calculate boundaries
             $v_min //= Vertex->new([@coordinates]);
             $v_max //= Vertex->new([@coordinates]);
-            for my $idx ( 0 .. @coordinates - 1) {
+            for (my $idx = 0 ; $idx < @coordinates; $idx++) {
                 $v_min->[$idx] = $coordinates[$idx] if $v_min->[$idx] > $coordinates[$idx];
                 $v_max->[$idx] = $coordinates[$idx] if $v_max->[$idx] < $coordinates[$idx];
             }
@@ -88,13 +88,13 @@ method load {
 
     my @vertex_incides;
     my @face_indices;
-    for my $face_idx (0 .. @faces-1) {
+    for (my $face_idx = 0 ; $face_idx < @faces; $face_idx++) {
         my $face_info = $faces[$face_idx];
         my $source_idx = $face_info->{v} - 1;
         my $info = $info_for_vertex{$source_idx};
         my ($f_indices, $n_indices, $t_indices) =  map { $info->{$_} } qw/faces normals textures/;
         my ($n, $t) = (-1, -1);
-        for my $j (0 .. @$n_indices-1) {
+        for (my $j = 0; $j < @$n_indices; $j++) {
             my $key = sub {
                 my ($n, $t) = ($_[0], $_[1] // '?');
                 return join("-", $source_idx, $n, $t);
