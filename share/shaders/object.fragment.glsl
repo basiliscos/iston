72e88adb32342e01f7c4d627a66c58c9d6616733
Ivan Baidakou <dmol@gmx.com>
2014-11-08 14:42:05 +0300

    Use Phong shading model instead of simplifyed Blinn-Phong

--- share/shaders/object.fragment.glsl
+++ share/shaders/object.fragment.glsl
@@ -46,7 +46,7 @@ material frontMaterial = material(
   vec4(0.75, 0.75, 0.75, 0),        // diffuse
   vec4(0.75, 0.75, 0.75, 0),        // ambient
   vec4(0.2, 0.2, 0.2, 1),           // specular
-  510.0                             // shininess
+  120.0                             // shininess
 );
 
 uniform vec4 default_color = vec4(0.8, 0.8, 0.8, 1);
@@ -57,9 +57,8 @@ uniform vec4 global_ambient = vec4(0.2, 0.2, 0.2, 1); // yellow abmient
 vec4 compute_color(vec4 initial_color) {
   if(has_lighting == 0) return initial_color;
   vec4 light_dir = normalize(light1.position);
-  vec3 light_dir3 = vec3(light_dir.x, light_dir.y, light_dir.z);
-  vec3 N = normalize(normal);
-  float intensity = max(dot(N, light_dir3), 0);
+  vec4 N = vec4(normalize(normal), 0.0);
+  float intensity = max(dot(N, light_dir), 0);
 
   vec4 diffuse = frontMaterial.diffuse * light1.diffuse;
   vec4 ambient = frontMaterial.ambient * (light1.ambient + global_ambient);
@@ -67,11 +66,10 @@ vec4 compute_color(vec4 initial_color) {
   vec4 specular = vec4(0.0);
 
   if (intensity > 0.0) {
-    vec4 eye = -1 * camera;
-    vec4 eye_shifted = view_model * eye;
-    vec4 halfway = normalize( eye - light_dir );
-    float NdotHV = max(dot(vec4(N, 0.0), halfway),0.0);
-    specular = light1.specular * frontMaterial.specular * pow(NdotHV, frontMaterial.shininess);
+    vec4 eye = -1 * normalize(camera);
+    vec4 R = normalize(dot(light_dir, N)) * 2 * N - eye;
+    float specular_intencity = max(dot(R, eye), 0.0);
+    specular = light1.specular * frontMaterial.specular * pow(specular_intencity, frontMaterial.shininess);
   }
 
   vec4 result = initial_color * (intensity * diffuse + ambient) + specular;
