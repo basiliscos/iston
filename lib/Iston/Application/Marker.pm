ea08424b73bdf32eff76a7a886e0b00dcd60f293
Ivan Baidakou <ivan.baidakou@pms-software.com>
2015-06-09 14:08:29 +0300

    Allow set zones and traverse them

--- lib/Iston/Application/Marker.pm
+++ lib/Iston/Application/Marker.pm
@@ -27,6 +27,10 @@ has models_path    => (is => 'ro', required => 1);
 has main_object    => (is => 'rw');
 has _commands      => (is => 'lazy');
 has _markers       => (is => 'lazy');
+has _current_zone  => (is => 'rw', default => sub { -1 });
+
+has xz_angle       => (is => 'rw', default => sub { 0 });
+has yz_angle       => (is => 'rw', default => sub { 0 });
 
 sub BUILD {
     my $self = shift;
@@ -39,24 +43,25 @@ sub _build__markers {
     my $mc = MarkerContainer->new(
         shader => $self->shader_for->{object},
     );
-    $mc->scale($self->max_boundary * 1.1);
-    push @{ $mc->zones }, Zone->new(
-        xz => 0,
-        yz => 0,
-        spread => 10,
-    );
+    $mc->scale($self->max_boundary + 0.1);
     return $mc;
 }
 
 sub objects {
     my $self = shift;
-    return [ $self->_markers, ($self->main_object ? ($self->main_object) : ())  ];
+    return [ $self->_markers, ($self->main_object ? ($self->main_object) : ()) ];
 }
 
 sub _load_model {
     my ($self, $model_path) = @_;
     say "loading $model_path";
     my $object = $self->load_object($model_path);
+
+    # my $r1 = ($object->radius) * $object->scale;
+    # my $r2 = $self->max_boundary;
+    # my $scale_to = $r1/$r2;
+    # $object->scale($scale_to);
+    # $object->clear_draw_function;
     $self->main_object($object);
     $self->settings_bar->refresh;
 }
@@ -99,11 +104,9 @@ sub _build_menu {
     my $direction_xz = [0.0, 0.0, 1.0];
     my $initial_direction_xz = [0.0, 0.0, 1.0];
     my $xz_start = Vector->new(values => [$initial_direction_xz->[0], 0, $initial_direction_xz->[2]]);
-    my $xz_angle = 0;
-    my $yz_angle = 0;
 
     my $rotate_objects = sub {
-        $self->_rotate($xz_angle, $yz_angle);
+        $self->_rotate($self->xz_angle, $self->yz_angle);
         $self->settings_bar->refresh;
     };
 
@@ -119,7 +122,7 @@ sub _build_menu {
             $direction_xz = Vector->new(values => $direction_xz)->normalize->values;
 
             my $xz_axis = Vector->new(values => [$direction_xz->[0], 0, $direction_xz->[2]]);
-            $xz_angle = $xz_axis->is_zero
+            my $xz_angle = $xz_axis->is_zero
                 ? 0
                 : do {
                     my $xz_angle = $xz_start->angle_with($xz_axis);
@@ -128,6 +131,7 @@ sub _build_menu {
                     $xz_angle *= $xz_sign;
                 };
             $xz_angle = rad2deg $xz_angle;
+            $self->xz_angle($xz_angle);
             $rotate_objects->();
         }
     );
@@ -135,7 +139,7 @@ sub _build_menu {
         mode       => 'ro',
         name       => "zx-angle",
         type       => 'number',
-        cb_read    => sub { $xz_angle },
+        cb_read    => sub { $self->xz_angle },
     );
 
     my $direction_yz = [0.0, 0.0, 1.0];
@@ -154,7 +158,7 @@ sub _build_menu {
             $direction_yz = Vector->new(values => $direction_yz)->normalize->values;
 
             my $yz_axis = Vector->new(values => [0, $direction_yz->[1], $direction_yz->[2]]);
-            $yz_angle = $yz_axis->is_zero
+            my $yz_angle = $yz_axis->is_zero
                 ? 0
                 : do {
                     my $yz_angle = $yz_start->angle_with($yz_axis);
@@ -164,6 +168,7 @@ sub _build_menu {
                 }
                 ;
             $yz_angle = rad2deg $yz_angle;
+            $self->yz_angle($yz_angle);
             $rotate_objects->();
         }
     );
@@ -171,44 +176,93 @@ sub _build_menu {
         mode       => 'ro',
         name       => "zy-angle",
         type       => 'number',
-        cb_read    => sub { $yz_angle },
+        cb_read    => sub { $self->yz_angle },
+    );
+    $bar->add_variable(
+        mode       => 'ro',
+        name       => "zones",
+        type       => 'number',
+        cb_read    => sub { scalar(@{ $self->_markers->zones }) },
+    );
+    $bar->add_variable(
+        mode       => 'ro',
+        name       => "current_zone",
+        type       => 'number',
+        cb_read    => sub { $self->_current_zone + 1 },
     );
 }
 
+sub _add_zone {
+    my ($self) = @_;
+    my $mc = $self->_markers;
+    push @{ $mc->zones }, Zone->new(
+        xz => -1 * $self->xz_angle,
+        yz => -1 * $self->yz_angle,
+        spread => 10,
+        active => 1,
+    );
+    if ($self->_current_zone >= 0) {
+        $mc->zones->[$self->_current_zone]->active(0);
+    }
+    $self->_current_zone( @{ $mc->zones } - 1 );
+    $self->settings_bar->refresh;
+    $mc->clear_draw_function;
+}
+
+sub _remove_zone {
+    my ($self) = @_;
+    my $idx = $self->_current_zone;
+    return unless $idx >= 0;
+    my $mc = $self->_markers;
+    splice @{ $mc->zones }, $idx, 1;
+    $idx--;
+    $idx = 0 if ($idx < 0 && @{ $mc->zones });
+    $self->_current_zone($idx);
+    $self->settings_bar->refresh;
+    $mc->clear_draw_function;
+}
+
+sub _activate_prev {
+    my ($self) = @_;
+    my $idx = $self->_current_zone;
+    my $mc = $self->_markers;
+    my $total = @{ $mc->zones };
+    return unless $idx >= 0;
+
+    $mc->zones->[$idx]->active(0);
+    $idx--;
+    $idx = $total - 1 if ($idx < 0);
+    $self->_current_zone($idx);
+    $mc->zones->[$idx]->active(1);
+    $self->settings_bar->refresh;
+    $mc->clear_draw_function;
+}
+
+sub _activate_next {
+    my ($self) = @_;
+    my $idx = $self->_current_zone;
+    my $mc = $self->_markers;
+    my $total = @{ $mc->zones };
+    return unless $idx >= 0;
+
+    $mc->zones->[$idx]->active(0);
+    $idx++;
+    $idx %= $total;
+    $self->_current_zone($idx);
+    $mc->zones->[$idx]->active(1);
+    $self->settings_bar->refresh;
+    $mc->clear_draw_function;
+}
+
+
 sub _build__commands {
-    my $self = shift;
-    my $rotation = sub {
-        my (%step_for) = @_; # key: axis, value: degree
-        my $subject = $self->main_object;
-        return sub {
-            while(my ($axis, $step) = each(%step_for)){
-                my $value = $subject->rotate($axis);
-                $value += $step;
-                $value %= 360;
-                $subject->rotate($axis, $value);
-            }
-        }
-    };
-    my $camera_z_move = sub {
-        my $value = shift;
-        return sub {
-            $self->camera_position->[2] += $value;
-            $self->_update_view;
-        };
-    };
-    my $rotate_step = 2;
+    my ($self) = @_;
     my $commands = {
-        'rotate_N'    => $rotation->(0, -$rotate_step),
-        'rotate_S'    => $rotation->(0, $rotate_step),
-        'rotate_W'    => $rotation->(1, -$rotate_step),
-        'rotate_E'    => $rotation->(1, $rotate_step),
-        'rotate_NW'   => $rotation->(0, -$rotate_step, 1, -$rotate_step),
-        'rotate_NE'   => $rotation->(0, -$rotate_step, 1, $rotate_step),
-        'rotate_SW'   => $rotation->(0, $rotate_step, 1, -$rotate_step),
-        'rotate_SE'   => $rotation->(0, $rotate_step, 1, $rotate_step),
-        'move_camera_forward'  => $camera_z_move->(0.1),
-        'move_camera_backward' => $camera_z_move->(-0.1),
-        'terminate_program'    => sub { $self->sdl_app->stop },
+        'add_zone'          => sub { $self->_add_zone; },
+        'remove_zone'       => sub { $self->_remove_zone; },
+        'activate_next'     => sub { $self->_activate_next; },
+        'activate_prev'     => sub { $self->_activate_prev; },
+        'terminate_program' => sub { $self->sdl_app->stop },
     };
     return $commands;
 }
@@ -218,6 +272,17 @@ sub process_event {
     # say "processing event...";
     my $action;
     AntTweakBar::eventSDL($event);
+    if ($event->type == SDL_KEYUP) {
+        my $dispatch_table = {
+            SDLK_SPACE()     => 'add_zone',
+            SDLK_BACKSPACE() => 'remove_zone',
+            SDLK_LEFT()      => 'activate_prev',
+            SDLK_RIGHT()     => 'activate_next',
+        };
+        my $key = $event->key_sym;
+        my $command = $dispatch_table->{$key};
+        $action = $self->_commands->{$command} if defined $command;
+    }
     if ($event->type == SDL_QUIT) {
         $action = $self->_commands->{'terminate_program'};
     }
---
+++ lib/Iston/Object/MarkerContainer.pm
@@ -0,0 +1,101 @@
+package Iston::Object::MarkerContainer;
+
+use 5.16.0;
+use warnings;
+
+use Function::Parameters qw(:strict);
+use Moo;
+use List::Util qw/all/;
+use OpenGL qw(:all);
+use Iston::Utils qw/as_oga maybe_zero/;
+
+has draw_function  => (is => 'lazy', clearer => 1);
+has zones => (is => 'ro', default => sub { [] });
+
+with('Iston::Drawable');
+
+method has_texture { return 0; };
+
+method _build_draw_function {
+    my $default_color = [102.0/255, 0.0, 204.0/255, 0.0];
+    my $active_color = [1.0, 1.0, 1.0, 0.0];
+
+    my $rotations = 16;
+    my $step = 360 / $rotations;
+
+    my @vertices = [0, 0, 0];
+    my @indices;
+    my @colors = ($default_color);
+    for my $zone_idx (0 .. @{ $self->zones }-1) {
+        my $zone = $self->zones->[$zone_idx];
+        my @zone_vertices = map {$_->values} $zone->sphere_points(0);
+        my $zone_center_idx = @vertices;
+        for (my $phi = $step; $phi < 360/2; $phi += $step) {
+            push @zone_vertices, map {$_->values} $zone->sphere_points($phi, 0);
+        }
+        push @vertices, @zone_vertices;
+        # connect every vertex with center on sphere
+        push @indices, map { ($zone_center_idx, $zone_center_idx + $_) } (1 .. @zone_vertices-1);
+        # connect every vertex with sphere center
+        push @indices, map { (0, $zone_center_idx + $_) } (1 .. @zone_vertices-1);
+        # connect every non-center vertex with neiborhood
+        push @indices, map {
+            map { $zone_center_idx + $_ } ($_+1, $_ +3), ($_+2, $_+4);
+        } (0 .. @zone_vertices-5);
+        # last positive with the 1st negative
+        push @indices, map { $_ + $zone_center_idx } ((@zone_vertices-2), 2);
+        # last negative with the 1st positive
+        push @indices, map { $_ + $zone_center_idx } ((@zone_vertices-1), 1);
+        my $color = $zone->active ? $active_color : $default_color;
+        push @colors, (($color) x @zone_vertices);
+    }
+
+    my ($vertices_oga, $colors_oga) = map { as_oga($_) } (\@vertices , \@colors);
+    my ($vbo_vertices, $vbo_colors) = glGenBuffersARB_p(2);
+
+    $vertices_oga->bind($vbo_vertices);
+    glBufferDataARB_p(GL_ARRAY_BUFFER_ARB, $vertices_oga, GL_STATIC_DRAW_ARB);
+
+    $colors_oga->bind($vbo_colors);
+    glBufferDataARB_p(GL_ARRAY_BUFFER_ARB, $colors_oga, GL_STATIC_DRAW_ARB);
+
+    my $draw_mode = GL_LINES;
+    my $indices_oga =OpenGL::Array->new_list(
+        GL_UNSIGNED_INT,
+        @indices,
+    );
+
+    my $indices_size = @indices;
+
+    $self->shader->Enable;
+    my $has_texture_u = $self->_uniform_for->{has_texture};
+    my $has_multicolor_u = $self->_uniform_for->{has_multicolor};
+    my $has_lighting_u = $self->_uniform_for->{has_lighting};
+    my $attribute_coord3d = $self->_attribute_for->{coord3d};
+    my $attribute_multicolor = $self->_attribute_for->{a_multicolor};
+    $self->shader->Disable;
+
+    return sub {
+        $self->shader->Enable;
+        glUniform1iARB($has_lighting_u, 0);
+        glUniform1iARB($has_texture_u, 0);
+        glUniform1iARB($has_multicolor_u, 1);
+
+        $self->shader->SetMatrix(model => $self->model_oga);
+
+        glEnableVertexAttribArrayARB($attribute_coord3d);
+        glBindBufferARB(GL_ARRAY_BUFFER, $vertices_oga->bound);
+        glVertexAttribPointerARB_c($attribute_coord3d, 3, GL_FLOAT, 0, 0, 0);
+
+        glEnableVertexAttribArrayARB($attribute_multicolor);
+        glBindBufferARB(GL_ARRAY_BUFFER, $colors_oga->bound);
+        glVertexAttribPointerARB_c($attribute_multicolor, 4, GL_FLOAT, 0, 0, 0);
+
+        glDrawElements_c(GL_LINES, $indices_size, GL_UNSIGNED_INT, $indices_oga->ptr);
+
+        glDisableVertexAttribArrayARB($_) for($attribute_coord3d, $attribute_multicolor);
+        $self->shader->Disable;
+    };
+};
+
+1;
